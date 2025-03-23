889923d9c506b98cd44e91f894c4a43beb12c167
stas <chousetheone@gmail.com>
2025-03-23 16:45:50 +0200

    url change

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,4 @@
-const AUTH_API_URL = "http://localhost:8080";
+const AUTH_API_URL = "http://202.182.126.241:8080";
 const BASE_URL = "http://localhost:8899";
 
 document.addEventListener("DOMContentLoaded", function() {
