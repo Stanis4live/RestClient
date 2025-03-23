eec2729e52ac8a89d83f543692de6f267c4003e8
stas <chousetheone@gmail.com>
2025-03-23 18:33:05 +0200

    img

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,4 @@
-const AUTH_API_URL = "https://stanis4live.su/api";
+const AUTH_API_URL = "https://stanis4live.su";
 const BASE_URL = "https://stanis4live.su";
 
 document.addEventListener("DOMContentLoaded", function() {
